118c69c289cec2beea1cbad0f031dea20d624529
Haruki07-crew <higaharu100@gmail.com>
2026-02-25 16:46:17 +0900

    スラッシュコマンドの追加

--- atcoder_function.py
+++ atcoder_function.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime, timezone, timedelta
 
+#現在のレートを取得
 def get_latest_rating(atcoder_name):
   url = f"https://atcoder.jp/users/{atcoder_name}/history/json"
   response = requests.get(url)
@@ -11,6 +12,7 @@ def get_latest_rating(atcoder_name):
   latest_rating = latest_contenst["NewRating"]
   return f"{atcoder_name}の現在のレートは{latest_rating}です"
 
+#これまでのAC数を取得
 def get_ac_count(atcoder_name):
   url_ac_sum = f"https://kenkoooo.com/atcoder/atcoder-api/v3/user/ac_rank?user={atcoder_name}"
   response_ac_sum = requests.get(url_ac_sum)
@@ -18,6 +20,7 @@ def get_ac_count(atcoder_name):
   ac_sum = data_ac_sum["count"]
   return ac_sum
 
+#今日のAC数を取得
 def count_today_ac(atcoder_name):
   time_difference = timezone(timedelta(hours=9))
   now = datetime.now(time_difference)
@@ -33,11 +36,22 @@ def count_today_ac(atcoder_name):
       s.add(submission["id"])
       ac_point_sum += submission["point"]
   return [len(s), ac_point_sum]
+
+#これまで・今日のAC数をまとめて返す
 def AC_print(atcoder_name):
   ac_sum = get_ac_count(atcoder_name)
   daily_ac_sum, daily_ac_point_sum = count_today_ac(atcoder_name)
   if daily_ac_sum == 0:
-    result = f"{atcoder_name}さんの今までのAC数は{ac_sum}\n今日のAC数は{daily_ac_sum}です。精進しましょう"
+    result = f"{atcoder_name}さんの今までのAC数は{ac_sum}\n今日のAC数は{daily_ac_sum}です。精進せんかい"
   else:
     result = f"{atcoder_name}さんの今までのAC数は{ac_sum}\n今日のAC数は{daily_ac_sum}で{daily_ac_point_sum}点取得しました"
   return result
+
+#登録されたユーザー間で今日のACの比較
+def AC_fight(user_name_dist):
+  result = []
+  for atcoder_name in user_name_dist:
+    ac_count = count_today_ac(atcoder_name)
+    result.append({"discord_name": user_name_dist[atcoder_name], "ac": ac_count})
+    sorted_result = sorted(result, key = lambda x: x["ac"], reverse=True)
+    return sorted_result
--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import discord
 from discord import app_commands
 import config
 import atcoder_function
-# 全ての情報を受け取る設定(サーバ情報、メンバー情報etc..)
 intents = discord.Intents.all()
-# clientはbot全体のオブジェクト(イベント(入室、メッセージの受信)の検知、メッセージの送信)
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-# asyncは非同期型 別の処理の完了を待たずに別の処理をする
+
+user_name_dist = {}
+
+
 @client.event
 async def on_ready():
   await tree.sync()
   print("bot起動!")
-  
-# message はdiscordで送信されたメッセージの情報を全て含んだオブジェクト
-user_name_dist={}
 
 @client.event
 async def on_message(message):
@@ -30,7 +28,64 @@ async def on_message(message):
     elif purpose == "AC":
       result = atcoder_function.AC_print(atcoder_name)
       await message.channel.send(result)
+#現在のレートの取得
+@tree.command(name = "rating", description="Atcoderのレートを取得します")
+async def rating_command(interaction: discord.Interaction, atcoder_name: str):
+  result = atcoder_function.get_latest_rating(atcoder_name)
+  await interaction.response.send_message(result)
+#これまでのAC数および今日のAC数
+@tree.command(name = "ac_count", description="AC数を取得します")
+async def AC_counter(interaction: discord.Interaction, atcoder_name: str):
+  result = atcoder_function.AC_print(atcoder_name)
+  await interaction.response.send_message(result)
+#ユーザーの登録
+@tree.command(name = "user_resister", description="ユーザーを登録します")
+async def user_resister(interaction: discord.Interaction, atcoder_name: str, discord_name: str):
+  user_name_dist[atcoder_name] = discord_name
+  await interaction.response.send_message(f"{discord_name}さんを{atcoder_name}で登録しました")
+#ユーザー登録の解除
+@tree.command(name = "user_unresister", description="登録されているユーザーの登録を解除します")
+async def user_unresister(interaction: discord.Interaction, atcoder_name: str):
+  if atcoder_name in user_name_dist:
+    discor_name = user_name_dist[atcoder_name]
+    del user_name_dist[atcoder_name]
+    await interaction.response.send_message(f"{discor_name}さんの登録を解除しました")
+#登録されているユーザの一覧を表示
+@tree.command(name = "user_list", description="登録済みユーザーの一覧を表示します")
+async def user_list(interaction: discord.Interaction):
+  if user_name_dist:
+    for atcoder_name in user_name_dist:
+      result = f"{user_name_dist[atcoder_name]}→{atcoder_name}"
+      await interaction.response.send_message(result)
+  else:
+    await interaction.response.send_message("登録されているユーザーがいません")
+#ユーザー同士でAC数を比較
+@tree.command(name = "ac_fight", description="ユーザー同士でACを比較することができます")
+async def ac_fight(interaction: discord.Interaction):
+  await interaction.response.send_message("ちょっと待てよ、時間かかるけん")
+  result = atcoder_function.AC_fight(user_name_dist)
+  if not result:
+    await interaction.response.send_message("登録されているユーザーがいません")
+    return
+  out_response = "🏆 AC fight ランキング🏆\n"
 
+  for i, dist in enumerate(result):
+    place = i + 1
+    if place == 1:
+      figure = "🥇 "
+    elif place == 2:
+      figure = "🥈 "
+    elif place == 3:
+      figure = "🥉 "
+    else:
+      figure = "🫵 "
+    award = f"{place}位 {dist["discord_name"]} {dist["ac"]}AC\n"
+    tmp_response = figure + award
+    out_response += tmp_response
+  await interaction.response.send_message(out_response) 
 
 
 client.run(config.DISCORD_TOKEN)
+
+
+
